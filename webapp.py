e1dfb741c1c87a37e6007bd417db693513e947bf
icarus7185 <163373986+icarus7185@users.noreply.github.com>
2026-01-20 11:47:11 +0000

    .

--- webapp.py
+++ webapp.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, Response
 from fastapi.responses import HTMLResponse, JSONResponse
 import io
 from pydantic import BaseModel
+import re
+import json
+from engine import GenericRuleEngine
+from report import ReportMaker
+import report
 
 app = FastAPI()
 
@@ -19,10 +24,26 @@ async def read_root():
 
 # --- API 1: Xử lý Prompt (Text -> Text) ---
 @app.get("/api/analyze")
-async def process_prompt(text: str = ""):
+async def process_prompt(input_source_code: str = ""):
+
+    rules = json.load(open('rules.json'))
+
+    tutor = GenericRuleEngine(rules)
+    print("--- Đang phân tích mã nguồn... ---")
+    issues = tutor.analyze(input_source_code)
+
+    short_messege = [item['message'] for item in issues]
+
+    print(short_messege)
+
+    # TẠO BÁO CÁO HTML
+    report = ReportMaker(input_source_code, issues)
+    report.generate_html_report("ket_qua_phan_tich.html")
+
     with open('ket_qua_phan_tich.html', 'r', encoding='utf-8') as file:
         html_content = file.read()
-
+        
+    print(html_content)
     return html_content
 
 @app.post("/api/analyze")
